f49dfe93cbef5c6bff83cd25efb7f11701f98257
Rishi Wadhwa <rishi@wadhwas.us>
2021-09-04 15:11:21 -0700

    Added simple JSON gradle dependency.

--- src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
+++ src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
@@ -1,4 +1,11 @@
 package frc.team3256.robot.auto.paths;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
 public class JSONReader {
 }
