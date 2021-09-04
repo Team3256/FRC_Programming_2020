20e7fecc1e12b98aee2e6e3ac4ae4d3c0cc3dccb
Rishi Wadhwa <rishi@wadhwas.us>
2021-09-04 16:26:36 -0700

    JSONReader.java functions and constructions
    JSON parses single-time 'translation' points from a designation JSON file defined in constructor and initialized in function.

--- src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
+++ src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
@@ -1,5 +1,6 @@
 package frc.team3256.robot.auto.paths;
 
+import java.util.Iterator;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,25 +10,43 @@ import java.util.Map;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
-public class JSONReader {
-    String file = "Hi";
-    Object obj;
+import javax.swing.text.html.HTMLDocument;
 
-    JSONObject jo;
-    public JSONReader() {
+public class JSONReader {
+    private String file; // file to read from
+    private Object obj; // JSONParser to parse json 'file'
+
+    public JSONReader(String file) {
+        this.file = file;
+    }
+
+    public void initJSONReader() {
         try {
              obj = new JSONParser().parse(new FileReader(file));
         } catch (Exception e) {
             System.out.println(e);
             return;
         }
-
-        jo = (JSONObject) obj;
     }
 
+    public void ParseJSONFile() {
+        JSONArray translation = (JSONArray) ((JSONObject) obj).get("translation"); // array of points
 
+        Iterator itr = ((Map) translation.iterator()).entrySet().iterator();
+        int counter = 0;
 
+        float x;
+        float y;
 
+        while (itr.hasNext()) {
+            Map.Entry pair = (Map.Entry) itr.next();
+            if (counter == 0) {
+                x = (float) pair.getValue(); // x-value
+            } else if (counter == 1) {
+                y = (float) pair.getValue(); // y-value
+            }
 
-
+            counter++;
+        }
+    }
 }
