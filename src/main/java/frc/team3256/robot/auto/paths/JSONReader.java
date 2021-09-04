bb9b377fa2943f4242eb01e13a3ca1ea8ffb956d
Silverleaf_2 <silverleaf.yen@gmail.com>
2021-09-04 15:55:55 -0700

    Initialize json parser

--- src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
+++ src/main/java/frc/team3256/robot/auto/paths/JSONReader.java
@@ -1,6 +1,8 @@
 package frc.team3256.robot.auto.paths;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -8,4 +10,24 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class JSONReader {
+    String file = "Hi";
+    Object obj;
+
+    JSONObject jo;
+    public JSONReader() {
+        try {
+             obj = new JSONParser().parse(new FileReader(file));
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        jo = (JSONObject) obj;
+    }
+
+
+
+
+
+
 }
